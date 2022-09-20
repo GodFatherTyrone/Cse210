d0f0d09eaf06e6877988834e18460e1db29f52a9
Ty Budvarson <tygunnarbud@gmail.com>
2022-09-20 16:01:23 -0600

    Created the beginning

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
+
+
+// See https://aka.ms/new-console-template for more information
+
